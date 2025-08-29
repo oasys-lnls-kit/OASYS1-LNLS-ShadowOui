3bb162a32c73ffc17a2ed1d3b6fb56947e5ec106
andriel24044 <andrielm02@gmail.com>
2025-08-29 13:14:24 -0300

    Compatibility fixes
    - Fixed the interaction between ShadowBeam and ShadowData objects in the fix_beam method.

--- orangecontrib/shadow/lnls/widgets/utility/ow_beam_analysis.py
+++ orangecontrib/shadow/lnls/widgets/utility/ow_beam_analysis.py
@@ -2,6 +2,7 @@ import copy
 import sys
 import time
 
+from matplotlib.patches import Shadow
 import numpy
 from PyQt5 import QtGui, QtWidgets
 from orangewidget import gui
@@ -11,6 +12,9 @@ from oasys.widgets import congruence
 from oasys.widgets.gui import ConfirmDialog
 
 from oasys.util.oasys_util import EmittingStream, TTYGrabber
+
+import Shadow
+from orangecontrib.shadow4.util.shadow4_objects import ShadowData
 
 from orangecontrib.shadow.util.shadow_objects import ShadowBeam
 from orangecontrib.shadow.util.shadow_util import ShadowCongruence, ShadowPlot
@@ -467,6 +471,21 @@ class PlotXY(AutomaticElement):
         self.axY  = self.figure.add_axes(rect_Y, sharey=self.ax2D)
         self.axT  = self.figure.add_axes(rect_T)
         
+    def fix_beam(self, input_beam):
+        """
+        Check the type of the beam and fix it if necessary.
+        """
+        if isinstance(input_beam, ShadowBeam):
+            return input_beam
+        
+        elif isinstance(input_beam, ShadowData):
+            beam4 = input_beam.shadow_data.beam
+            beam3 = Shadow.Beam(N=input_beam.shadow_data.get_number_of_rays())
+            beam3.rays = beam4.rays.copy()
+            beam3.rays[:, 0:3] /= input_beam.workspace_units_to_m
+            beam3.rays[:, 12] /= input_beam.workspace_units_to_m
+            output_beam3 = ShadowBeam(beam=beam3, number_of_rays=input_beam.shadow_data.get_number_of_rays())
+        return output_beam3
 
     def save_fig(self):
         self.figure.savefig(self.output_filename, dpi=200)
