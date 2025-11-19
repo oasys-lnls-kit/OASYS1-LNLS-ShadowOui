6c43b619d32bd34aac78e5576c71a6bbc380c63e
giovanni.ribeiro <giovanni.ribeiro@lnls.br>
2025-11-19 10:36:47 -0300

    Update version to include ILUM students' contributions
    Main changes:
    - Beam analysis updated using latest optlnls `plot_beam` improvements
    - Caustic widget: 3D visualization rebuilt with PyVista
    - Flux widget: quality-of-life improvements and bug fixes
    
    Credits: Lucas Silva and Andriel Silva (ILUM)

--- setup.py
+++ setup.py
@@ -2,58 +2,72 @@
 
 import imp
 import os
-import sys
 import subprocess
+import sys
 
-NAME = 'OASYS1-LNLS-ShadowOui'
+NAME = "OASYS1-LNLS-ShadowOui"
 
-VERSION = '0.2.2'
+VERSION = "0.2.3"
 ISRELEASED = False
 
-DESCRIPTION = 'WIDGETS DEVELOPED FOR LNLS TO EXTEND SHADOWOUI FUNCTIONALITIES'
-README_FILE = os.path.join(os.path.dirname(__file__), 'README.txt')
+DESCRIPTION = "WIDGETS DEVELOPED FOR LNLS TO EXTEND SHADOWOUI FUNCTIONALITIES"
+README_FILE = os.path.join(os.path.dirname(__file__), "README.txt")
 LONG_DESCRIPTION = open(README_FILE).read()
-AUTHOR = 'Sergio Augusto Lordano Luiz, Artur Clarindo Pinto, Bernd Christian Meyer'
-AUTHOR_EMAIL = 'artur.pinto@lnls.br'
-URL = 'http://github.com/oasys-lnls-kit/OASYS1-LNLS-ShadowOui'
-DOWNLOAD_URL = 'http://github.com/oasys-lnls-kit/OASYS1-LNLS-ShadowOui'
-LICENSE = 'MIT'
+AUTHOR = "Sergio Augusto Lordano Luiz, Artur Clarindo Pinto, Bernd Christian Meyer"
+AUTHOR_EMAIL = "artur.pinto@lnls.br"
+URL = "http://github.com/oasys-lnls-kit/OASYS1-LNLS-ShadowOui"
+DOWNLOAD_URL = "http://github.com/oasys-lnls-kit/OASYS1-LNLS-ShadowOui"
+LICENSE = "MIT"
 
 KEYWORDS = (
-    'ray-tracing',
-    'simulator',
-    'oasys1',
+    "ray-tracing",
+    "simulator",
+    "oasys1",
 )
 
 CLASSIFIERS = (
-    'Development Status :: 4 - Beta',
-    'Environment :: X11 Applications :: Qt',
-    'Environment :: Console',
-    'Environment :: Plugins',
-    'Programming Language :: Cython',
-    'Programming Language :: Python',
-    'Programming Language :: Python :: 3',
-    'Intended Audience :: Science/Research',
+    "Development Status :: 4 - Beta",
+    "Environment :: X11 Applications :: Qt",
+    "Environment :: Console",
+    "Environment :: Plugins",
+    "Programming Language :: Cython",
+    "Programming Language :: Python",
+    "Programming Language :: Python :: 3",
+    "Intended Audience :: Science/Research",
 )
 
 
-SETUP_REQUIRES = (
-                  'setuptools',
-                  )
+SETUP_REQUIRES = ("setuptools",)
 
 INSTALL_REQUIRES = (
-                    'setuptools',
-                    'optlnls>=0.3.3',
-                   )
+    "setuptools",
+    "optlnls>=0.3.3",
+)
 
-if len({'develop', 'release', 'bdist_egg', 'bdist_rpm', 'bdist_wininst',
-        'install_egg_info', 'build_sphinx', 'egg_info', 'easy_install',
-        'upload', 'test'}.intersection(sys.argv)) > 0:
+if (
+    len(
+        {
+            "develop",
+            "release",
+            "bdist_egg",
+            "bdist_rpm",
+            "bdist_wininst",
+            "install_egg_info",
+            "build_sphinx",
+            "egg_info",
+            "easy_install",
+            "upload",
+            "test",
+        }.intersection(sys.argv)
+    )
+    > 0
+):
     import setuptools
+
     extra_setuptools_args = dict(
         zip_safe=False,  # the package can run out of an .egg file
         include_package_data=True,
-        install_requires=INSTALL_REQUIRES
+        install_requires=INSTALL_REQUIRES,
     )
 else:
     extra_setuptools_args = dict()
@@ -61,54 +75,64 @@ else:
 from setuptools import find_packages, setup
 
 PACKAGES = find_packages(
-                         exclude = ('*.tests', '*.tests.*', 'tests.*', 'tests'),
-                         )
+    exclude=("*.tests", "*.tests.*", "tests.*", "tests"),
+)
 
-PACKAGE_DATA = {"orangecontrib.shadow.lnls.widgets.utility":["icons/*.png", "icons/*.jpg"],
-                "orangecontrib.shadow.lnls.data":["data/*.*"],
+PACKAGE_DATA = {
+    "orangecontrib.shadow.lnls.widgets.utility": ["icons/*.png", "icons/*.jpg"],
+    "orangecontrib.shadow.lnls.data": ["data/*.*"],
 }
 
-NAMESPACE_PACKAGES = ["orangecontrib","orangecontrib.shadow", "orangecontrib.shadow.lnls", "orangecontrib.shadow.lnls.widgets"]
+NAMESPACE_PACKAGES = [
+    "orangecontrib",
+    "orangecontrib.shadow",
+    "orangecontrib.shadow.lnls",
+    "orangecontrib.shadow.lnls.widgets",
+]
 
 ENTRY_POINTS = {
-    'oasys.addons' : ("LNLS ShadowOui = orangecontrib.shadow.lnls", ),
-    'oasys.widgets' : (      
+    "oasys.addons": ("LNLS ShadowOui = orangecontrib.shadow.lnls",),
+    "oasys.widgets": (
         "Shadow LNLS Utility = orangecontrib.shadow.lnls.widgets.utility",
     ),
-    'oasys.menus' : ("shadowlnlsmenu = orangecontrib.shadow.lnls.menu",)
+    "oasys.menus": ("shadowlnlsmenu = orangecontrib.shadow.lnls.menu",),
 }
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     is_beta = False
 
     try:
-        import PyMca5, PyQt4
+        import PyMca5
+        import PyQt4
 
         is_beta = True
     except:
         setup(
-              name = NAME,
-              version = VERSION,
-              description = DESCRIPTION,
-              long_description = LONG_DESCRIPTION,
-              author = AUTHOR,
-              author_email = AUTHOR_EMAIL,
-              url = URL,
-              download_url = DOWNLOAD_URL,
-              license = LICENSE,
-              keywords = KEYWORDS,
-              classifiers = CLASSIFIERS,
-              packages = PACKAGES,
-              package_data = PACKAGE_DATA,
-              #          py_modules = PY_MODULES,
-              setup_requires = SETUP_REQUIRES,
-              install_requires = INSTALL_REQUIRES,
-              #extras_require = EXTRAS_REQUIRE,
-              #dependency_links = DEPENDENCY_LINKS,
-              entry_points = ENTRY_POINTS,
-              namespace_packages=NAMESPACE_PACKAGES,
-              include_package_data = True,
-              zip_safe = False,
-              )
+            name=NAME,
+            version=VERSION,
+            description=DESCRIPTION,
+            long_description=LONG_DESCRIPTION,
+            author=AUTHOR,
+            author_email=AUTHOR_EMAIL,
+            url=URL,
+            download_url=DOWNLOAD_URL,
+            license=LICENSE,
+            keywords=KEYWORDS,
+            classifiers=CLASSIFIERS,
+            packages=PACKAGES,
+            package_data=PACKAGE_DATA,
+            #          py_modules = PY_MODULES,
+            setup_requires=SETUP_REQUIRES,
+            install_requires=INSTALL_REQUIRES,
+            # extras_require = EXTRAS_REQUIRE,
+            # dependency_links = DEPENDENCY_LINKS,
+            entry_points=ENTRY_POINTS,
+            namespace_packages=NAMESPACE_PACKAGES,
+            include_package_data=True,
+            zip_safe=False,
+        )
 
-    if is_beta: raise NotImplementedError("This version of <APP NAME> doesn't work with Oasys1 beta.\nPlease install OASYS1 final release: http://www.elettra.eu/oasys.html")
+    if is_beta:
+        raise NotImplementedError(
+            "This version of <APP NAME> doesn't work with Oasys1 beta.\nPlease install OASYS1 final release: http://www.elettra.eu/oasys.html"
+        )
